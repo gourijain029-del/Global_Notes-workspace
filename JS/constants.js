74cc1e666da5d2fa24f6b374fdb10936366df402
ANEEK DAS <aneek22112007@gmail.com>
2025-11-21 14:45:44 +0530

    Delete JS/constants.js
    deleting

--- JS/constants.js
+++
@@ -1,5 +0,0 @@
-export const NOTES_STORAGE_PREFIX = "notesWorkspace.notes";
-export const ACTIVE_USER_KEY = "notesWorkspace.activeUser";
-export const ACCOUNT_KEY = "notesWorkspace.accounts.v1";
-
-
